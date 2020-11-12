477ec4cd480f71c593c5d3c7dac204dc9f50c1ce
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-12 23:02:05 +0100

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutSection from '../components/aboutSection';
+
+const About = () => {
+  return (
+    <>
+      <AboutSection />
+    </>
+  );
+};
+
+export default About;
