96fbf115bd360ee8e863c17f622aa9f9217e7209
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-18 16:31:48 +0100

    faqSection and servicesSection

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutSection from '../components/aboutSection';
+import ServicesSection from '../components/servicesSection';
 
 const About = () => {
   return (
     <>
       <AboutSection />
+      <ServicesSection />
     </>
   );
 };
