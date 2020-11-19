5daf119fe2bc30136d006348732194dbc519d071
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-19 21:52:41 +0100

    imported pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,29 @@ import GlobalStyle from './components/GlobalStyle';
 
 // import components
 import About from './pages/about';
+import ContactUs from './pages/contactUs';
+import OurWork from './pages/ourWork';
+import Nav from './components/nav';
+
+// Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <About />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <About />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
