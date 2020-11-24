079033f995ed8740378ad23043a27f01b1293075
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-24 23:05:32 +0100

    added scroll effect

--- src/components/servicesSection.js
+++ src/components/servicesSection.js
@@ -1,47 +1,54 @@
 import React from 'react';
-
+//Import Icons
 import clock from '../img/clock.svg';
 import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
-
-// Styles
+//Styles
 import { About, Description, Image } from '../styles';
 import styled from 'styled-components';
+import { scrollReveal } from '../animation';
+import { useScroll } from './useScroll';
 
 const ServicesSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Services>
+    <Services
+      variants={scrollReveal}
+      animate={controls}
+      initial="hidden"
+      ref={element}
+    >
       <Description>
         <h2>
-          High <span>quality</span>services
+          High <span>quality</span> services
         </h2>
         <Cards>
           <Card>
             <div className="icon">
-              <img src={clock} alt="clock" />
+              <img alt="icon" src={clock} />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-              <img src={teamwork} alt="teamwork" />
+              <img alt="icon" src={teamwork} />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-              <img src={diaphragm} alt="diaphragm" />
+              <img alt="icon" src={diaphragm} />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-              <img src={money} alt="money" />
+              <img alt="icon" src={money} />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
@@ -49,7 +56,7 @@ const ServicesSection = () => {
         </Cards>
       </Description>
       <Image>
-        <img src={home2} alt="home2" />
+        <img alt="camera" src={home2} />
       </Image>
     </Services>
   );
@@ -64,23 +71,22 @@ const Services = styled(About)`
     padding: 2rem 0rem 4rem 0rem;
   }
 `;
-
 const Cards = styled.div`
   display: flex;
   flex-wrap: wrap;
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
 `;
-
 const Card = styled.div`
   flex-basis: 20rem;
-
   .icon {
     display: flex;
     align-items: center;
-
     h3 {
       margin-left: 1rem;
-      background: #fff;
-      color: #000;
+      background: white;
+      color: black;
       padding: 1rem;
     }
   }
