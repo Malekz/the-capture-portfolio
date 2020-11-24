eb3ed5efe01a2884440881915ba6bb126b378677
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-24 22:26:22 +0100

    styling about page and imported scrollTop

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,27 @@
 import React from 'react';
+//Page Components
 import AboutSection from '../components/aboutSection';
 import ServicesSection from '../components/servicesSection';
 import FaqSection from '../components/faqSection';
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
+import ScrollTop from '../components/scrollTop';
 
-const About = () => {
+const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <AboutSection />
       <ServicesSection />
       <FaqSection />
-    </>
+      <ScrollTop />
+    </motion.div>
   );
 };
 
-export default About;
+export default AboutUs;
