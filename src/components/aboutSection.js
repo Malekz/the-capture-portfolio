63020f9c2cf465c5f9126fd602c863c8855450c9
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-24 22:05:40 +0100

    import framer-motion

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -2,26 +2,28 @@ import React from 'react';
 import home1 from '../img/home1.png';
 
 // Styled
-//import styled from 'styled-components';
 import { About, Description, Image, Hide } from '../styles';
+
+// Framer Motion
+import { motion } from 'framer-motion';
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
-          <Hide className="hide">
-            <h2>We work to make</h2>
+        <motion.div>
+          <Hide>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
-          <div className="hide">
-            <h2>
-              your <span>dreams</span>come
-            </h2>
-          </div>
-          <Hide className="hide">
-            <h2>true.</h2>
+          <Hide>
+            <motion.h2>
+              your <span>dreams</span> come
+            </motion.h2>
           </Hide>
-        </div>
+          <Hide className="hide">
+            <motion.h2>true.</motion.h2>
+          </Hide>
+        </motion.div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nihil
           assumenda officia repudiandae. Obcaecati consequatur distinctio nisi,
