df2004949a6ec7308a6c82a97ad3c3b6790bcda4
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-24 22:39:49 +0100

    used animatePresence from framer-motion and useLocation from react-dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import React from 'react';
-
-//import Global Style
+//Global Style
 import GlobalStyle from './components/GlobalStyle';
-
-// import components
-import About from './pages/about';
+//Import Pages
+import AboutUs from './pages/about';
 import ContactUs from './pages/contactUs';
 import OurWork from './pages/ourWork';
 import Nav from './components/nav';
 import MovieDetail from './pages/movieDetail';
-// Router
-import { Switch, Route } from 'react-router-dom';
+//Router
+import { Switch, Route, useLocation } from 'react-router-dom';
+//Animation
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <GlobalStyle />
+
       <Nav />
-      <Switch>
-        <Route exact path="/">
-          <About />
-        </Route>
-        <Route exact path="/work">
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
